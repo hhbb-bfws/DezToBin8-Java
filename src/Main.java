e060827aff6d6c34cd47c021da1cf7bbd6f09073
hh <hh@emailz.advprog.de>
2019-01-24 11:42:30 +0100

    Methode eingefügt

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,11 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
-        try
-        {
+    public static void main(String[] args) {
+        try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             Console console = System.console();
-            if(console == null)
-            {
+            if (console == null) {
                 System.out.println("No console available");
                 //return;
             }
@@ -21,50 +18,46 @@ public class Main {
             System.out.println("\nDezToBin8");
             String strEnde = "";
 
-            while (strEnde != "e")
-            {
+            while (strEnde != "e") {
                 System.out.print("\nBitte Zahl im Dezimalsystem eingegebn (0..255): ");
                 String strDez = reader.readLine();
                 //String strZahl = System.console().readLine();
 
+                DezToBin8(strDez);
 
-                Integer nDez = Integer.parseInt(strDez);
-
-                if ((nDez >= 0) && (nDez < 256))
-                {
-                    String strBin = "";
-                    Integer nMod = 0;
-                    Integer nDiv = nDez;
-
-                    for (int i=0; i<8; i++)
-                    {
-                        nMod = nDiv % 2;
-                        nDiv = nDiv / 2;
-                        strBin = nMod.toString() + strBin;
-                    }
-
-                    System.out.println(strDez + " lautet im Binärsystem: " + strBin);
-                }
-                else if (nDez < 0)
-                {
-                    System.out.println("Fehler, Zahl zu klein!");
-                }
-                else
-                {
-                    System.out.println("Fehler, Zahl zu groß!");
-                }
-
-                System.out.print("Programm be(e)nden? ");
+                System.out.print("\nProgramm be(e)nden? ");
                 strEnde = reader.readLine();
             }
 
-        }
-        catch(Exception ex)
-        {
+        } catch (Exception ex) {
             System.out.println(ex.toString());
         }
 
 
+    }
 
+    private static String DezToBin8(String strDez) {
+        String strBin = "";
+        Integer nDez = Integer.parseInt(strDez);
+
+
+        if ((nDez >= 0) && (nDez < 256)) {
+
+            Integer nMod = 0;
+            Integer nDiv = nDez;
+
+            for (int i = 0; i < 8; i++) {
+                nMod = nDiv % 2;
+                nDiv = nDiv / 2;
+                strBin = nMod.toString() + strBin;
+            }
+
+            System.out.println(strDez + " lautet im Binärsystem: " + strBin);
+        } else if (nDez < 0) {
+            System.out.println("Fehler, Zahl zu klein!");
+        } else {
+            System.out.println("Fehler, Zahl zu groß!");
+        }
+        return strBin;
     }
 }
